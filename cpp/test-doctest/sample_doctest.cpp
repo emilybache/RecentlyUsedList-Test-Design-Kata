c19f16d242b95b8247bd37e108e8afa28a0d16bf
emily <emily@bacheconsulting.com>
2022-03-18 13:39:49 +0100

    sample solution with test fixture

--- cpp/test-doctest/sample_doctest.cpp
+++ cpp/test-doctest/sample_doctest.cpp
@@ -4,13 +4,12 @@
 
 #include "recently_used_list.h"
 
-
-void LookupPages(Queue* q, Hash* hash, int pages[], int length)
-{
-    for (int i = 0; i < length ; i ++)
-    {
+static Queue *createAndFillQueue(Hash *hash, int queue_length, int *pages, int pages_length) {
+    Queue* q = createQueue(queue_length);
+    for (int i = 0; i < pages_length; i++) {
         LookupPage(q, hash, pages[i]);
     }
+    return q;
 }
 
 void EXPECT_QUEUE_CONTENTS(int expected[], int actual[], int length)
@@ -21,50 +20,55 @@ void EXPECT_QUEUE_CONTENTS(int expected[], int actual[], int length)
     }
 }
 
-TEST_CASE ("Recently Used List") {
+TEST_CASE ("Recently Used List")
+{
     Hash* hash = createHash(10);
 
-
-
     SUBCASE("Four elements") {
-        int length = 4;
-        Queue* q = createQueue(length);
+        int queue_length = 4;
         int pages[] = {1,2,3,1,4,5};
-        LookupPages(q, hash, pages, 6);
+        int pages_length = 6;
+
+        Queue *q = createAndFillQueue(hash, queue_length, pages, pages_length);
 
         int expected[] = {5, 4, 1, 3};
         int actual[] = {0, 0, 0, 0};
-        currentPageOrder(q, actual, length);
-        EXPECT_QUEUE_CONTENTS(expected, actual, length);
+        currentPageOrder(q, actual, queue_length);
+        EXPECT_QUEUE_CONTENTS(expected, actual, queue_length);
     }
 
     SUBCASE("removeOneNotFromBack")
     {
-        int length = 3;
-        Queue* q = createQueue(length);
+        int queue_length = 3;
 
         int pages[] = {1,2,3,2,4,5};
-        LookupPages(q, hash, pages, 6);
+        int pages_length = 6;
+
+        Queue* q = createAndFillQueue(hash, queue_length, pages, pages_length);
 
         int expected[] = {5, 4, 2};
         int actual[] = {0, 0, 0};
-        currentPageOrder(q, actual, length);
-        EXPECT_QUEUE_CONTENTS(expected, actual, length);
+        currentPageOrder(q, actual, queue_length);
+        EXPECT_QUEUE_CONTENTS(expected, actual, queue_length);
     }
 
     SUBCASE("oneElement")
     {
-        Queue* q = createQueue(1);
-        int length = 1;
-
+        int queue_length = 1;
         int pages[] = {1,2,3};
-        LookupPages(q, hash, pages, 3);
+        int pages_length = 3;
+
+        Queue* q = createAndFillQueue(hash, queue_length, pages, pages_length);
 
         int expected[] = {3};
         int actual[] = {0};
-        currentPageOrder(q, actual, length);
-        EXPECT_QUEUE_CONTENTS(expected, actual, length);
+        currentPageOrder(q, actual, queue_length);
+        EXPECT_QUEUE_CONTENTS(expected, actual, queue_length);
     }
 }
+
+
+
+
 
 
