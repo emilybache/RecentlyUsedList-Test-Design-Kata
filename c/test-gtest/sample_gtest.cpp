6e0734cc1a385d45cfbd9a347d0ae55ec4a01064
emily <emily@bacheconsulting.com>
2023-01-22 13:53:03 +0100

    sample solution C++

--- c/test-gtest/sample_gtest.cpp
+++ c/test-gtest/sample_gtest.cpp
@@ -10,16 +10,13 @@ TEST(RecentlyUsedListTest, fourPages)
 {
     int length = 4;
     Queue* q = createQueue(length);
-
     Hash* hash = createHash(10);
 
     // Let us look up pages 1, 2, 3, 1, 4, 5
-    LookupPage(q, hash, 1);
-    LookupPage(q, hash, 2);
-    LookupPage(q, hash, 3);
-    LookupPage(q, hash, 1);
-    LookupPage(q, hash, 4);
-    LookupPage(q, hash, 5);
+    int pages[] = {1, 2, 3, 1, 4, 5};
+    for (int i = 0; i < 6; ++i) {
+        LookupPage(q, hash, pages[i]);
+    }
 
     int expected[] = {5, 4, 1, 3};
     int actual[] = {0, 0, 0, 0};
@@ -33,7 +30,6 @@ TEST(RecentlyUsedList, removeOneNotFromBack)
 {
     int length = 3;
     Queue* q = createQueue(length);
-
     Hash* hash = createHash(10);
 
     LookupPage(q, hash, 1);
--- cpp/test-gtest/sample_gtest.cpp
+++ cpp/test-gtest/sample_gtest.cpp
@@ -3,9 +3,40 @@
 #include "recently_used_list.h"
 
 using namespace std;
-using namespace ::testing;
 
-TEST(RecentlyUsedListTest, something)
-{
-    EXPECT_EQ(true, false);
+class RecentlyUsedListTest : public ::testing::Test {
+public:
+    RecentlyUsedList* rul = new RecentlyUsedList();
+};
+
+TEST_F(RecentlyUsedListTest, Empty) {
+    auto expected = new vector<string>();
+    ASSERT_EQ(rul->getContents(), *expected);
+}
+
+TEST_F(RecentlyUsedListTest, OneItem) {
+    rul->insert("item");
+    auto expected = new vector<string>{"item"};
+    ASSERT_EQ(rul->getContents(), *expected);
+}
+
+TEST_F(RecentlyUsedListTest, TwoItemsOrderedByInsertion) {
+    rul->insert("item1");
+    rul->insert("item2");
+    auto expected = new vector<string>{"item2", "item1"};
+    ASSERT_EQ(rul->getContents(), *expected);
+}
+
+TEST_F(RecentlyUsedListTest, DuplicateItemsAreMovedNotInserted) {
+    rul->insert("item1");
+    rul->insert("item2");
+    rul->insert("item1");
+    auto expected = new vector<string>{"item1", "item2"};
+    ASSERT_EQ(rul->getContents(), *expected);
+}
+
+TEST_F(RecentlyUsedListTest, EmptyStringsAreNotAllowed) {
+    rul->insert("");
+    auto expected = new vector<string>{};
+    ASSERT_EQ(rul->getContents(), *expected);
 }
